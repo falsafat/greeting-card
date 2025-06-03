b69b2518244884a1f0b3e4f79f4e91fc0f9ee1df
falsafat <62481836+falsafat@users.noreply.github.com>
2025-06-03 20:49:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ async function generateCard() {
 
     // إحداثيات النص (يمكن تعديلها حسب حاجتك)
     const xPos = canvas.width * 0.83;
-    const yPos = canvas.height * 0.85;
+    const yPos = canvas.height * 0.89;
 
     // كتابة الاسم
     ctx.fillText(userName, xPos, yPos);
